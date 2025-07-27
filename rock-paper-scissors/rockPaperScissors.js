616dac9604a5e0a79ac239df872eaa04b7f8837b
Büsra Demirhan <demhan.busra@gmail.com>
2025-07-27 14:17:42 +0200

    Updated the game: Rock, Paper, Scissors

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -1,1 +1,27 @@
-console.log("Rock, Paper, Scissors game is starting...")
+const playerMove = process.argv.slice(2)[0];
+
+const moves = ['rock', 'paper', 'scissors'];
+let randomMove = Math.floor(Math.random() * moves.length)
+const computerMove = moves[randomMove]
+
+let score = null
+
+if (!playerMove) {
+    console.log("Please provide your move! Rock, Paper or Scissors");
+
+} else {
+    console.log(`Your choose: ${playerMove}`)
+}
+
+
+if (computerMove) {
+    console.log(`The computers choose: ${computerMove}`)
+}
+
+if (playerMove === computerMove) {
+    console.log(`It's a draw!`)  
+} else if (playerMove === 'rock' && computerMove === 'scissors' || playerMove === 'paper' && computerMove === 'rock' || playerMove === 'scissors' && computerMove === 'paper') {
+    console.log(`You win!`)
+} else {
+    console.log(`The computer wins!`)
+}
